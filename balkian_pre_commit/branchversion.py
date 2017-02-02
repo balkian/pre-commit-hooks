b7dc27a616c151af0a2b9a35c9767a4230c72563
J. Fernando Sánchez <balkian@gmail.com>
2017-02-02 04:30:31 +0100

    Make branchversion run always
    This should prevent commits without VERSION bump

--- balkian_pre_commit/branchversion.py
+++ balkian_pre_commit/branchversion.py
@@ -8,17 +8,19 @@ def main(argv=None):
     parser.add_argument('filenames', nargs='*')
     args = parser.parse_args(argv)
     branch_name = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode()
+    error = 1
     for fn in args.filenames:
-        if os.path.basename(fn) == 'VERSION':
-            with open(fn) as f:
-                currentcontent = f.read().strip()
-                prevcontent = check_output(['git', 'show', 'HEAD:{}'.format(fn)]).strip().decode()
-                current = semver.parse_version_info(currentcontent)
-                print('Checking versions: {} -> {}'.format(prevcontent, currentcontent))
-                if (branch_name == 'master' and current.prerelease) or\
-                   (semver.compare(currentcontent, prevcontent)<1):
-                    return 1
-    return 0
+        with open(fn) as f:
+            currentcontent = f.read().strip()
+            prevcontent = check_output(['git', 'show', 'HEAD:{}'.format(fn)]).strip().decode()
+            current = semver.parse_version_info(currentcontent)
+            print('Checking versions: {} -> {}'.format(prevcontent, currentcontent))
+            if (branch_name == 'master' and current.prerelease) or\
+                (semver.compare(currentcontent, prevcontent)<1):
+                return 1
+            else:
+                error = 0
+    return error
 
 if __name__ == '__main__':
     exit(main())
